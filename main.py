75fd4445c2f51a06922eac9348b96a44fd79c185
Noman <mail.nomanraihan@gmail.com>
2025-08-12 08:15:45 +0600

    Code with comments

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+# The arr contains random numbers that are unsorted
+# We need to find the maximum number in it.
+# Assume that the first element of the arr is the maximum number
+
+
 arr = [5, 4, 8, 7, 2]
+
+# We are assuming that the first element of the arr is maximum number
 max_num = arr[0]
 
+
 for i in arr:
-    if i > max_num:
-        max_num = i
+    if i > max_num:     # Check each element and compare
+        max_num = i     # During loop, if an element is found larger then the max_num variable will be updated
 
 
 print(max_num)
